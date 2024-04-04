d4d799669f3f1a7779e7f2363b503a1b871bace2
Leo42night <karmaborutovvo@gmail.com>
2024-04-05 00:48:34 +0700

    Create ProductSeeder.php

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'kode' => Str::random(10),
+            'name' => Str::random(10),
+            'price' => '',
+            'stock' => ''
+        ]);
+    }
+}

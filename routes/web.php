b0ab17dca9c6663a91b7df172eab45c5ea9302f7
Leo42night <karmaborutovvo@gmail.com>
2024-04-04 22:11:21 +0700

    dasar migrasi

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [ProductController::class, 'index'])->name('index');
+Route::get('/products', [ProductController::class, 'index'])->name('index');
+Route::get('products/create', [ProductController::class,'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('products/{id}', [ProductController::class,'destroy'])->name('products.destroy');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/brand', function(){
+  return view('brand');
+})->name('brand');
+
+Route::get('/categories', function(){
+  return view('categories');
+})->name('categories');

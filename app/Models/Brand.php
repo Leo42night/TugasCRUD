ba8b1737f2ff36e510459e01c4ac895576a1353e
Leo42night <karmaborutovvo@gmail.com>
2024-04-05 00:27:53 +0700

    model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'img',
+        'name',
+        'website',
+        'address'
+    ];
 }
